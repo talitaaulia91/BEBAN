e330e4fd580f12d54aceeb822d211abd293bb83e
faraahsm <85088236+faraahsm@users.noreply.github.com>
2021-12-24 08:34:15 +0700

    add model dan controller

---
+++ app/Http/Controllers/login.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('/login', [
+            "title" => "Login"
+        ]);
+    }
+
+    public function authenticate(Request $request)
+    {
+      
+        $credentials = [
+            'username' => $request->email,
+            'password' => $request->password,
+        ];
+
+        if(Auth::guard('mahasiswa')->attempt($credentials)){
+            $request->session()->regenerate();
+            return redirect()->intended(route('dashboard'));
+        }
+        return back()->with(['loginError' => 'Username atau Password salah']);
+    }
+
+    public function logout(Request $request){
+        if(Auth::guard('mahasiswa')->check()){
+            Auth::logout();
+            $request->session()->invalidate();
+            $request->session()->regenerateToken();
+            return redirect('/');
+        }
+    }
+}
